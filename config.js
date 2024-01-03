d617cf9988ea21a82fbfd3a96fb98542ff08890f
hirushadenuwan11 <153351356+hirushadenuwan11@users.noreply.github.com>
2024-01-03 15:57:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94714334610)', 'IT KOLLA²⁰²³', true],
+  ['94714334610', 'IT KOLLA²⁰²³', true], 
   [''] 
 ] //Number of owners
 
